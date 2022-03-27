e66b03c428405863797767233833b21b35c1c79c
xValeryx <101188945+xValeryx@users.noreply.github.com>
2022-03-27 16:38:15 +0200

    Add files via upload

--- 21-03 esercizi/rubrica.js
+++ 21-03 esercizi/rubrica.js
@@ -16,18 +16,18 @@ const name = q("#name");
 const surname = q("#surname");
 const telefono = q("#number");
 const add = q("button");
-const del = q("delete");
+
 
 const myRubrica = [ 
     
-   "Giovanni Boccaccio - Tel. 3472436456",
-   "Francesco Petrarca - Tel. 3402455643",
-   "Dante Alighieri - Tel. 340246542",
-   "Giacomo Leopardi - Tel. 3233546554",
-   "Alessandro Manzoni - Tel. 33223435",
-   "Elsa Morante - Tel. 3475666298",
-   "Alma Sabatini - Tel. 347889463",
-   "Dacia Maraini - 3274546573",
+   "Giovanni - Boccaccio - Tel. 3472436456",
+   "Francesco - Petrarca - Tel. 3402455643",
+   "Dante - Alighieri - Tel. 340246542",
+   "Giacomo - Leopardi - Tel. 3233546554",
+   "Alessandro - Manzoni - Tel. 33223435",
+   "Elsa - Morante - Tel. 3475666298",
+   "Alma - Sabatini - Tel. 347889463",
+   "Dacia - Maraini - Tel. 3274546573",
 
 ];
 
@@ -45,6 +45,7 @@ const render = (value = "") => {
         (person) => `<li class="card">
             <span class="name">${person[0]}</span>
             <span class="surname">${person[1]}</span>
+            <span class="telefono">${person[2]}</span>
             
           </li>`
       )
@@ -54,35 +55,66 @@ const render = (value = "") => {
 render();
 
 // Aggiungere un contatto 
+
 add.addEventListener("click", () => {
     myRubrica.push(`${name.value} - ${surname.value} - ${number.value}`);
     render();
 
     name.value = "";
     surname.value = "";
-    number.value = "";
+    telefono.value = "";
 
 });
 
 
 // Cercare i contatti
+
 search.addEventListener("keyup", () => render(search.value));
+
 
 // Eliminare un contatto
 
-function delContact() {
-    const index = parseInt(prompt("Inserisci l'indice"));
-    myRubrica.splice(index, 1);
-    console.log(personList);
-}
+/*
 
-del.addEventListener("click", () => {
- myRubrica.splice(0, 1);
- render();
- 
+const remove = document.querySelectorAll("li");
 
-});
+  try {
+    remove.forEach((card) => {
+      card.addEventListener("click", () => {
+        const index = card.getAttribute("data-index");
+        addresses.splice(index, 1);
+        localStorage.setItem("addresses", JSON.stringify(addresses));
+        myRubrica();
+      });
+    });
+  } catch (error) {
+    const p = document.createElement("p");
+    newContactBtn.insertAdjacentHTML(
+      "afterend",
+      "<p class = 'error'>Attenzione, errore nell'aggiunta del contatto!</p>"
+    );
+  }
+
+  
+}; 
+
+*/   
+
+const deleteLiElContent = (element) => {
+    element.addEventListener("click", () => {
+       element.textContent = "";
+  
+      ulEl.removeChild(element);
+  
+      element.classList.toggle("li-text");
+    
+    });
+};
+
+const liEls = document.querySelectorAll("li");
+
+liEls.forEach((liEl) => deleteLiElContent(liEl));
 
 
-render();
+
  
